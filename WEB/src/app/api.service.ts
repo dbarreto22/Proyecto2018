b6cc2c7d21d6a4f8de148eab85a499352eeda90a
dbarreto22 <ddiegob2212@gmail.com>
2018-11-26 23:36:50 -0300

    actualizando

--- WEB/src/app/api.service.ts
+++ WEB/src/app/api.service.ts
@@ -33,9 +33,9 @@ paramsCalificaciones = paramsCalificaciones.append('idAsig_Carrera', localStorag
 
 @Injectable()
 export class ApiService {
-   API_URL  = 'http://localhost:8080/miudelar-server'; 
+  // API_URL  = 'http://localhost:8080/miudelar-server'; 
   // url = 'http://localhost:8080/miudelar-server/director/carrera/';  
-  //API_URL = 'http://b0b9853a.ngrok.io/miudelar-server'; 
+  API_URL = 'http://3f5c1846.ngrok.io/miudelar-server'; 
   //url = 'http://b0b9853a.ngrok.io/miudelar-server/director/carrera/';  
     //'http://tsi-diego.eastus.cloudapp.azure.com:8080/miudelar-server';
 
